db152b4616a2eedeb35513b424a15092d23bc851
tony <anthonyherrera24@gmail.com>
2018-03-15 09:19:03 -0500

    Updated vimrc. Added script templates. Need to go over vimrc and bashrc and make it look better. It has kind of gotten out of hand.

---
+++ scripts/bash_template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Name example.sh
+# Purpose example boilerplate
+#
+# Author Full Name: User@EMail.com
+# Revision  Month XX 20XX - initial version
+
+
+
---
+++ scripts/python_template.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Name example.sh
+# Purpose example boilerplate
+#
+# Author Full Name: User@EMail.com
+# Revision  Month XX 20XX - initial version
+
+
+
