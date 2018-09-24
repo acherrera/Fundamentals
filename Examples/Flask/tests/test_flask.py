35867d6c8e0e3b5f97bb6ca734ff05b308fb4188
Anthony Herrera <anthonyherrera24@gmail.com>
2018-09-24 11:00:23 -0500

    updated with statis flask example

--- Examples/Flask/tests/test_flask.py
+++ Examples/Flask/tests/test_flask.py
@@ -1,12 +1,9 @@
 
 from unittest import mock, TestCase
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../modules")
 import main.py
 
 
-class TeatFlask(TestCase):
+class TestFlask(TestCase):
 
     def test_index(self):
-
+        pass
